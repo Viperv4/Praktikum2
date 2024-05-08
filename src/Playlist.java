e3f442d3db66b1d426a62be6e53de9f84d4a7284
Stefan Kerkenpaß <s.kerkenpass@t-online.de>
2024-05-08 12:01:14 +0200

    Pr2.3

--- src/Playlist.java
+++ src/Playlist.java
@@ -2,47 +2,57 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PlayList {
-    private LinkedList list = new LinkedList<>();
-
-    public PlayList() {
-    }
-
-    public PlayList(String m3uPathname) {
-    }
-
-    public void add(AudioFile file) {
-        list.add(file);
-    }
-
-    public void remove(AudioFile file) {
-        list.remove(file);
-    }
-
-    public int size() {
-        return list.toArray().length;
-    }
-
-    public int getCurrent() {
-        return 0;
-    }
-
-    public void setCurrent(int current) {
-    }
-
-    public AudioFile currentAudioFile() {
-        return null;
-    }
-
-    public void nextSong() {
-    }
-
-    public void loadFromM3U(String pathname) {
-    }
-
-    public void saveAsM3U(String pathname) {
-    }
-
-    public List<AudioFile> getList() {
-        return list;
-    }
+	private LinkedList list = new LinkedList<>();
+	private int current;
+	
+	public PlayList() {
+	}
+	
+	public PlayList(String m3uPathname) {
+	}
+	
+	public void add(AudioFile file) {
+		list.add(file);
+	}
+	
+	public void remove(AudioFile file) {
+		list.remove(file);
+	}
+	
+	public int size() {
+		return list.toArray().length;
+	}
+	
+	public int getCurrent() {
+		return current;
+	}
+	
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+	
+	public AudioFile currentAudioFile() {
+		try {
+			return (AudioFile) list.get(current);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	public void nextSong() {
+		current++;
+		if (current >= list.toArray().length || current < 0) {
+			current = 0;
+		}
+	}
+	
+	public void loadFromM3U(String pathname) {
+	}
+	
+	public void saveAsM3U(String pathname) {
+	}
+	
+	public List<AudioFile> getList() {
+		return list;
+	}
 }
