3437d6506362d18e18ecdcc121ccbb91e159c689
Stefan Kerkenpaß <s.kerkenpass@t-online.de>
2024-05-28 11:59:57 +0200

    Pr2.4

--- src/studiplayer/audio/AlbumComparator.java
+++ src/studiplayer/audio/AlbumComparator.java
@@ -3,39 +3,15 @@ package studiplayer.audio;
 import java.util.Comparator;
 
 public class AlbumComparator implements Comparator<AudioFile> {
+	
 	@Override
 	public int compare(AudioFile o1, AudioFile o2) {
-		TaggedFile o11 = null;
-		TaggedFile o21 = null;
-		WavFile o12 = null;
-		WavFile o22 = null;
-		int num1 = 0;
-		int num2 = 0;
-		if (o1.getClass() == TaggedFile.class) {
-			o11 = (TaggedFile) o1;
-			num1 = 1;
-		} else {
-			o12 = (WavFile) o1;
-			num1 = 2;
-		}
-		
-		if (o1.getClass() == TaggedFile.class) {
-			o21 = (TaggedFile) o2;
-			num2 = 1;
-		} else {
-			o22 = (WavFile) o2;
-			num2 = 2;
-		}
-		if (num1 == 1 && num2 == 1) {
+		try {
+			TaggedFile o11 = (TaggedFile) o1;
+			TaggedFile o21 = (TaggedFile) o2;
 			return o11.getAlbum().compareTo(o21.getAlbum());
+		} catch (Exception e) {
 		}
-//		  else if (num1 == 1 && num2 == 2) {
-//			return o11.getAlbum().compareTo(o22.getAlbum());
-//		} else if (num1 == 2 && num2 == 1) {
-//			return o12.getAlbum().compareTo(o21.getAlbum());
-//		} else if (num1 == 2 && num2 == 2) {
-//			return o12.getAlbum().compareTo(o22.getAlbum());
-//		}
-		return 0;
+		return -1;
 	}
 }
--- src/studiplayer/audio/DurationComparator.java
+++ src/studiplayer/audio/DurationComparator.java
@@ -5,60 +5,18 @@ import java.util.Comparator;
 public class DurationComparator implements Comparator<AudioFile> {
 	@Override
 	public int compare(AudioFile o1, AudioFile o2) {
-		TaggedFile o11 = null;
-		TaggedFile o21 = null;
-		WavFile o12 = null;
-		WavFile o22 = null;
-		int num1 = 0;
-		int num2 = 0;
-		if (o1.getClass() == TaggedFile.class) {
-			o11 = (TaggedFile) o1;
-			num1 = 1;
-		} else {
-			o12 = (WavFile) o1;
-			num1 = 2;
+		try {
+			SampledFile o11 = (SampledFile) o1;
+			SampledFile o21 = (SampledFile) o2;
+			if (o11.getDuration() < o21.getDuration()) {
+				return -1;
+			} else if (o11.getDuration() == o21.getDuration()) {
+				return 0;
+			} else {
+				return 1;
+			}
+		} catch (Exception e) {
 		}
-		
-		if (o1.getClass() == TaggedFile.class) {
-			o21 = (TaggedFile) o2;
-			num2 = 1;
-		} else {
-			o22 = (WavFile) o2;
-			num2 = 2;
-		}
-		if (num1 == 1 && num2 == 1) {
-			if (o11.getDuration() < o12.getDuration()) {
-				return -1;
-			} else if (o11.getDuration() == o12.getDuration()) {
-				return 0;
-			} else {
-				return 1;
-			}
-		} else if (num1 == 1 && num2 == 2) {
-			if (o11.getDuration() < o22.getDuration()) {
-				return -1;
-			} else if (o11.getDuration() == o12.getDuration()) {
-				return 0;
-			} else {
-				return 1;
-			}
-		} else if (num1 == 2 && num2 == 1) {
-			if (o11.getDuration() < o12.getDuration()) {
-				return -1;
-			} else if (o11.getDuration() == o12.getDuration()) {
-				return 0;
-			} else {
-				return 1;
-			}
-		} else if (num1 == 2 && num2 == 2) {
-			if (o11.getDuration() < o12.getDuration()) {
-				return -1;
-			} else if (o11.getDuration() == o12.getDuration()) {
-				return 0;
-			} else {
-				return 1;
-			}
-		}
-		return 0;
+		return -1;
 	}
 }
