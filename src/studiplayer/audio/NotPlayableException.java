376e2cf286739f768bb39d15dc405cba29c6b9d4
Stefan Kerkenpaß <s.kerkenpass@t-online.de>
2024-05-29 10:19:01 +0200

    Pr2.4

--- src/studiplayer/audio/NotPlayableException.java
+++ src/studiplayer/audio/NotPlayableException.java
@@ -3,17 +3,17 @@ package studiplayer.audio;
 public class NotPlayableException extends Exception {
 	private String path;
 	
-	NotPlayableException(String pathname, String msg) {
+	public NotPlayableException(String pathname, String msg) {
 		super(msg);
 		path = pathname;
 	}
 	
-	NotPlayableException(String pathname, Throwable t) {
+	public NotPlayableException(String pathname, Throwable t) {
 		super(t);
 		path = pathname;
 	}
 	
-	NotPlayableException(String pathname, String msg, Throwable t) {
+	public NotPlayableException(String pathname, String msg, Throwable t) {
 		super(msg, t);
 		path = pathname;
 	}
