d3e33cbeb0dcbb0baa3c462c86b158355e435bea
Stefan Kerkenpaß <s.kerkenpass@t-online.de>
2024-06-05 16:33:02 +0200

    Pr2.5

---
+++ src/studiplayer/ui/Player.java
@@ -0,0 +1,20 @@
+package studiplayer.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Player extends Application {
+	public Player() {
+	}
+	
+	
+	public static void main(String[] args) {
+		launch();
+	}
+	
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.setTitle("Balls");
+	}
+}
